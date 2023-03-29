764fb979d4b8e2bbedbd6f413bb8d6515a400958
kodego-mae <funj451@gmail.com>
2023-03-29 19:15:11 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const body = document.querySelector("body");
+const button = document.querySelector("button");
+
+function random(number) {
+  return Math.floor(Math.random() * number);
+}
+function changeBackground() {
+  body.style.backgroundColor = `rgb(${random(255)}, ${random(255)},${random(
+    255
+  )})`;
+}
+
+body.addEventListener("click", changeBackground);
